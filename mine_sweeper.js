c9c47b62ff220c91dcd1c5aa504988cd147b4a00
rimmesbe <ryanimmesberger@gmail.com>
2016-07-25 16:58:03 -0400

    got html board generating in the mineSweeper object

--- mine_sweeper.js
+++ mine_sweeper.js
@@ -1,9 +1,26 @@
 "use strict";
+var _table_ = document.createElement('table'),
+    _tr_ = document.createElement('tr'),
+    _td_ = document.createElement('td');
 
 function MineSweeper(size) {
   var results = "";
-  this.board = new Board(size);
+  this.mineBoard = new Board(size);
+  this.board = this.mineBoard.board;
   this.setResults = function(status) {
     results = status;
   };
+}
+
+MineSweeper.prototype.generateBoard = function() {
+  var table = $("table");
+  for(var row=0; row<this.board.length; row++) {
+    var tr = _tr_.cloneNode(false);
+    for(var col=0; col<this.board[row].length; col++) {
+      var td = _td_.cloneNode(false);
+      $(td).text(this.board[row][col]);
+      $(tr).append(td);
+    }
+    $(table).append(tr);
+  }
 }
