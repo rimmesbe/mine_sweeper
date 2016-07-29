9f34d1c280dbce65fc0e7ccf65c506efcf4aba58
rimmesbe <ryanimmesberger@gmail.com>
2016-07-29 18:15:06 -0400

    got recursive spot reveal working

--- mine_sweeper.js
+++ mine_sweeper.js
@@ -4,10 +4,47 @@ var _table_ = document.createElement('table'),
     _td_ = document.createElement('td'),
     _span_ = document.createElement('span');
 
+function findSpot(row, col){
+  var spot = $('#'+row+"r").find('#'+col+"c").find('span');
+  return spot;
+}
+
+// reveals location, recurses if adjacent spot has 0 bomb count
+function reveal(target) {
+  target.addClass('revealed');
+  if($(target).text() === "B"){
+    console.log("You Looooose!");
+    $('table').unbind('click');
+  } else if($(target).text() === "0") {
+    var targetX = parseInt($(target).parent().parent().attr('id').charAt(0)); //row id
+    var targetY = parseInt($(target).parent().attr('id').charAt(0)); //col id
+    var startX, startY, endX, endY;
+
+    targetX === 0 ? startX = 0 : startX = -1;
+    targetY === 0 ? startY = 0 : startY = -1;
+    targetX === parseInt($('tr').last().attr('id').charAt(0)) ? endX = 1 : endX = 2;
+    targetY === parseInt($('td').last().attr('id').charAt(0)) ? endY = 1 : endY = 2;
+
+    for(var x = startX; x < endX; x++) {
+      for(var y = startY; y < endY; y++) {
+        var currentSpot = findSpot((targetX+x), (targetY+y));
+        if($(currentSpot).text() === "0" && !($(currentSpot).hasClass("revealed"))) {
+          reveal(currentSpot);
+        }
+        if($(currentSpot).text() !== "B" && (!(y===0 && x===0))) {
+          $(currentSpot).addClass('revealed');
+        }
+      }
+    }
+  }
+  return;
+};
+
 function MineSweeper(size) {
-  var results = "";
+  var results = "unfinished";
   this.mineBoard = new Board(size);
   this.board = this.mineBoard.board;
+  this.getResults = function() {return results;};
   this.setResults = function(status) {
     results = status;
   };
@@ -17,10 +54,10 @@ MineSweeper.prototype.generateBoard = function() {
   var table = $("table");
   for(var row=0; row<this.board.length; row++) {
     var tr = _tr_.cloneNode(false);
-    $(tr).attr('id', row);
+    $(tr).attr('id', (row+"r"));
     for(var col=0; col<this.board[row].length; col++) {
       var td = _td_.cloneNode(false);
-      $(td).attr('id', col);
+      $(td).attr('id', (col+"c"));
       var span = _span_.cloneNode(false);
       $(span).text(this.board[row][col]);
       $(td).append(span);
@@ -30,13 +67,14 @@ MineSweeper.prototype.generateBoard = function() {
   }
 }
 
-MineSweeper.prototype.reveal = function() {
-  $('table').on('click', 'td', function(){
-    $(this).find('span').addClass('revealed');
-  })
+MineSweeper.prototype.userClick = function() {
+    $('table').on('click', 'td', function(){
+      var target = $(this).find('span');
+      reveal(target);
+    });
+};
 
 
-}
 
 $(document).ready(function(){
   var mine = new MineSweeper(10);
@@ -44,5 +82,6 @@ $(document).ready(function(){
   mine.mineBoard.calculateBoard();
   mine.generateBoard();
 
-  mine.reveal();
+  mine.userClick();
+
 });
