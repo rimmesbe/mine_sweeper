c09c742b23bf6b3aeeafa9dfde18db525558372e
rimmesbe <ryanimmesberger@gmail.com>
2016-07-29 20:36:32 -0400

    implemented a MineSweeper Module with public api

--- mine_sweeper.js
+++ mine_sweeper.js
@@ -4,71 +4,90 @@ var _table_ = document.createElement('table'),
     _td_ = document.createElement('td'),
     _span_ = document.createElement('span');
 
-function findSpot(row, col){
-  var spot = $('#'+row+"r").find('#'+col+"c").find('span');
-  return spot;
-}
+var MineSweeper = (function() {
 
-// reveals location, recurses if adjacent spot has 0 bomb count
-function reveal(target) {
-  target.addClass('revealed');
-  if($(target).text() === "B"){
-    console.log("You Looooose!");
-    $('table').unbind('click');
-  } else if($(target).text() === "0") {
-    var targetX = parseInt($(target).parent().parent().attr('id').charAt(0)); //row id
-    var targetY = parseInt($(target).parent().attr('id').charAt(0)); //col id
-    var startX, startY, endX, endY;
+  function findSpot(row, col){
+    var spot = $('#'+row+"r").find('#'+col+"c").find('span');
+    return spot;
+  }
 
-    targetX === 0 ? startX = 0 : startX = -1;
-    targetY === 0 ? startY = 0 : startY = -1;
-    targetX === parseInt($('tr').last().attr('id').charAt(0)) ? endX = 1 : endX = 2;
-    targetY === parseInt($('td').last().attr('id').charAt(0)) ? endY = 1 : endY = 2;
+  // reveals location, recurses if adjacent spot has 0 bomb count
+  function reveal(target) {
+    target.addClass('revealed');
+    if($(target).text() === "B"){
+      console.log("You Looooose!");
+      $('table').unbind('click');
+    } else if($(target).text() === "0") {
+      var targetX = parseInt($(target).parent().parent().attr('id').charAt(0)); //row id
+      var targetY = parseInt($(target).parent().attr('id').charAt(0)); //col id
+      var startX, startY, endX, endY;
 
-    for(var x = startX; x < endX; x++) {
-      for(var y = startY; y < endY; y++) {
-        var currentSpot = findSpot((targetX+x), (targetY+y));
-        if($(currentSpot).text() === "0" && !($(currentSpot).hasClass("revealed"))) {
-          reveal(currentSpot);
-        }
-        if($(currentSpot).text() !== "B" && (!(y===0 && x===0))) {
-          $(currentSpot).addClass('revealed');
+      targetX === 0 ? startX = 0 : startX = -1;
+      targetY === 0 ? startY = 0 : startY = -1;
+      targetX === parseInt($('tr').last().attr('id').charAt(0)) ? endX = 1 : endX = 2;
+      targetY === parseInt($('td').last().attr('id').charAt(0)) ? endY = 1 : endY = 2;
+
+      for(var x = startX; x < endX; x++) {
+        for(var y = startY; y < endY; y++) {
+          var currentSpot = findSpot((targetX+x), (targetY+y));
+          if($(currentSpot).text() === "0" && !($(currentSpot).hasClass("revealed"))) {
+            reveal(currentSpot);
+          }
+          if($(currentSpot).text() !== "B" && (!(y===0 && x===0))) {
+            $(currentSpot).addClass('revealed');
+          }
         }
       }
     }
-  }
-  return;
-};
+    return;
+  };
 
-function generateBoard() {
-  var table = $("table");
-  for(var row=0; row<this.board.length; row++) {
-    var tr = _tr_.cloneNode(false);
-    $(tr).attr('id', (row+"r"));
-    for(var col=0; col<this.board[row].length; col++) {
-      var td = _td_.cloneNode(false);
-      $(td).attr('id', (col+"c"));
-      var span = _span_.cloneNode(false);
-      $(span).text(this.board[row][col].val);
-      $(td).append(span);
-      $(tr).append(td);
+  function generateBoard() {
+    var table = $("table");
+    for(var row=0; row<board.length; row++) {
+      var tr = _tr_.cloneNode(false);
+      $(tr).attr('id', (row+"r"));
+      for(var col=0; col<board[row].length; col++) {
+        var td = _td_.cloneNode(false);
+        $(td).attr('id', (col+"c"));
+        var span = _span_.cloneNode(false);
+        $(span).text(board[row][col].val);
+        $(td).append(span);
+        $(tr).append(td);
+      }
+      $(table).append(tr);
     }
-    $(table).append(tr);
   }
-}
 
-function userClick = function() {
-    $('table').on('click', 'td', function(){
-      var target = $(this).find('span');
-      reveal(target);
-    });
-};
+  function userClick() {
+      $('table').on('click', 'td', function(){
+        var target = $(this).find('span');
+        reveal(target);
+      });
+  };
+
+  function init(){
+    game = new Board(10);
+    board = game.board;
+    game.seedBoard();
+    game.calculateBoard();
+  }
+
+  var
+    board,
+    game,
+    publicApi = {
+      init: init,
+      generateBoard: generateBoard,
+      userClick: userClick
+    }
+  ;
+
+  return publicApi;
+})();
 
 $(document).ready(function(){
-  var board = new Board(10);
-  board.seedBoard();
-  board.calculateBoard();
-  generateBoard();
-
-  userClick();
+  MineSweeper.init();
+  MineSweeper.generateBoard();
+  MineSweeper.userClick();
 });
